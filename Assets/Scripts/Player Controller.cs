935379cbb58a9c8a438e64706872651cb866de20
hyn335 <hyn335@naver.com>
2025-09-12 17:56:36 +0900

    카메라 함수 입력

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class   PlayerController : MonoBehaviour
 {
     [Header("이동 설정")]
     public float walkSpeed = 3.0f;
