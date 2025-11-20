4176ed6fb743c2c6c4d8fb0bb7e655aa2443498b
hyn335 <hyn335@naver.com>
2025-11-20 11:26:34 +0900

    업적 일부 코딩 입력

---
+++ Assets/Scripts/Achievement Type.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AchievementType 
+
+{
+   CollectCoins,              //코인 수집
+   CompleteQuests,           //퀘스트 완료
+   CollectItems,            //아이템 수집
+    TalkToNPCs,             //NPC와 대화
+
+}
---
+++ Assets/Scripts/AchievementData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Achievement", menuName = "Achievement/Achievement Data")]
+public class AchievementData : ScriptableObject
+
+{
+    public string achivevmentName;
+    public string description;
+    public AchievementType achievementType;
+    public int requiredAmount;
+    public int rewardCoins;
+    public bool isUnlocked;
+    public Sprite icon;
+}
